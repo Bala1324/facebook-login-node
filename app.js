8148c2f9012005394e349ab0b005bddfe5ec5468
Bala1324 <balachandiran132@gmail.com>
2021-09-09 10:07:11 +0530

    first Commit

--- app.js
+++ app.js
@@ -17,48 +17,41 @@ app.use(passport.initialize());
 
 passport.use(new facebookStrategy({
 
-    // pull in our app id and secret from our auth.js file
     clientID        : "4238968086183482",
     clientSecret    : "638c681fe774400b9337e80c210de1bc",
     callbackURL     : "http://localhost:5000/facebook/callback",
     profileFields: ['id', 'displayName', 'name', 'gender', 'picture.type(large)','email']
 
-},// facebook will send back the token and profile
+},
 function(token, refreshToken, profile, done) {
 
-    // asynchronous
+  
     process.nextTick(function() {
 
-        // find the user in the database based on their facebook id
         User.findOne({ 'uid' : profile.id }, function(err, user) {
 
-            // if there is an error, stop everything and return that
-            // ie an error connecting to the database
+            
             if (err)
                 return done(err);
 
-            // if the user is found, then log them in
+            
             if (user) {
                 console.log("user found")
                 console.log(user)
-                return done(null, user); // user found, return that user
+                return done(null, user); 
             } else {
-                // if there is no user found with that facebook id, create them
-                var newUser            = new User();
-
-                // set all of the facebook information in our user model
-                newUser.uid    = profile.id; // set the users facebook id                   
-                newUser.token = token; // we will save the token that facebook provides to the user                    
-                newUser.name  = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
-                newUser.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+            
+                var newUser = new User();
+                newUser.uid    = profile.id;                    
+                newUser.token = token;                     
+                newUser.name  = profile.name.givenName + ' ' + profile.name.familyName; 
+                newUser.email = profile.emails[0].value; 
                 newUser.gender = profile.gender
                 newUser.pic = profile.photos[0].value
-                // save our user to the database
+                
                 newUser.save(function(err) {
                     if (err)
                         throw err;
-
-                    // if successful, return the new user
                     return done(null, newUser);
                 });
             }
@@ -73,7 +66,7 @@ passport.serializeUser(function(user, done) {
     done(null, user.id);
 });
 
-// used to deserialize the user
+
 passport.deserializeUser(function(id, done) {
     User.findById(id, function(err, user) {
         done(err, user);
@@ -83,7 +76,7 @@ passport.deserializeUser(function(id, done) {
 app.get('/profile', isLoggedIn, function(req, res) {
     console.log(req.user)
     res.render('profile', {
-        user : req.user // get the user out of session and pass to template
+        user : req.user 
     });
 });
 
@@ -92,14 +85,12 @@ app.get('/logout', function(req, res) {
     res.redirect('/');
 });
 
-// route middleware to make sure
 function isLoggedIn(req, res, next) {
 
-	// if user is authenticated in the session, carry on
-	if (req.isAuthenticated())
+ if (req.isAuthenticated())
 		return next();
 
-	// if they aren't redirect them to the home page
+
 	res.redirect('/');
 }
 
@@ -116,5 +107,5 @@ app.get('/',(req,res) => {
 })
 
 app.listen(5000,() => {
-    console.log("App is listening on Port 5000")
+    console.log("listening Port 5000")
 })
